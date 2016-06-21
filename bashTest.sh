ed9141acbc1b7f9e9e18cba73cc492493be50231
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-21 11:53:21 -0500

    quick add

--- bashTest.sh
+++ bashTest.sh
@@ -1,3 +1,10 @@
-echo "Hello World1!"
-echo "Hello World2!"
-echo "Hello World3!"
+echo "Hello World!"
+echo "some data for the file" >> /Users/HenryHall/Documents/terminalAutomation/test.txt
+cd
+cd Documents/
+cd terminalAutomation/
+ls
+git add .
+git commit -m "quick add"
+git push -u origin master
+echo "Goodbye World!""
