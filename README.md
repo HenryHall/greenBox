5a737e5612a114a6f5fe936120d026a656929dce
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-21 12:08:45 -0500

    quick add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #Get those green blocks
+
+Initialize:
+
+Change line 4 of bashTest.sh to your local path
--- bashTest.sh
+++ bashTest.sh
@@ -1,10 +1,10 @@
 echo "Hello World!"
 echo DATE=`date +%Y-%m-%d` >> /Users/HenryHall/Documents/terminalAutomation/test.txt
 cd
-cd Documents/
-cd terminalAutomation/
+cd Documents/terminalAutomation/
 ls
+
 git add .
 git commit -m "quick add"
 git push -u origin master
-echo "Goodbye World!""
+echo "Goodbye World!"
